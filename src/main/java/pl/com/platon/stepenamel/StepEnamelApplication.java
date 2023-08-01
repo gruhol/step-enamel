590aceb190194aec86f0348f16911e44033463ce
Wojciech Dąbrowski <dabrowskiw@gmail.com>
2023-08-01 15:26:52 +0200

    Start projektct.

---
+++ src/main/java/pl/com/platon/stepenamel/StepEnamelApplication.java
@@ -0,0 +1,13 @@
+package pl.com.platon.stepenamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepEnamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepEnamelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/com/platon/stepenamel/StepEnamelApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.platon.stepenamel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepEnamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
